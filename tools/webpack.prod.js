348931685975a3e409b4dd0a674d4de2e13759c8
Orange Mi <orangemiwj@gmail.com>
2018-05-25 23:46:18 +0800

    update webpack

--- tools/webpack.prod.js
+++ tools/webpack.prod.js
@@ -6,14 +6,14 @@ const webpackMerge = require('webpack-merge')
 const AssetsPlugin = require('assets-webpack-plugin')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+// const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 const buildConfig = require('./webpack.common')
 
 module.exports = webpackMerge(buildConfig, {
   bail: true,
   output: {
-    path: path.resolve(__dirname, '../dist'),
+    path: path.resolve(__dirname, '../build'),
     publicPath: '/build/',
     filename: '[chunkhash].js',
     pathinfo: false
@@ -30,12 +30,12 @@ module.exports = webpackMerge(buildConfig, {
       parallel: true,
       extractComments: false
     }),
-    new HtmlWebpackPlugin({
-      template: 'src/index.ejs'
-    }),
+    // new HtmlWebpackPlugin({
+    //   template: 'src/index.ejs'
+    // }),
     new AssetsPlugin({
       filename: 'assets.json',
-      path: path.resolve(__dirname, '../dist'),
+      path: path.resolve(__dirname, '../build'),
       prettyPrint: true
     })
   ]
